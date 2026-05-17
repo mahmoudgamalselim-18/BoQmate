8fa2b2cdde0d4f1374cbb1d710b8b167b55b68a4
mahmoudgamalselim-18 <mahmoudgamalselim@gmail.com>
2026-05-18 01:28:51 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,10 @@
 export const metadata = {
-  title: "BOQmate  AI-Powered Construction Pricing",
+  title: "BOQmate — AI-Powered Construction Pricing",
   description: "AI-Powered Construction Pricing",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
